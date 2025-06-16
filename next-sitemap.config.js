b56dbd2c7fb678a4a8f8cfaad12f82cbd70a988f
Irfanrf <irfanrahmat083@gmail.com>
2025-06-16 15:10:56 +0700

    git commit -m "feat: add sitemap.xml with next-sitemap"

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+// next-sitemap.config.js
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://masterprima.site',
+  generateRobotsTxt: true,
+  exclude: ['/auth-mp-2024'], 
+};
